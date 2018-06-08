32e4cd6e8b03eef2236c21f99e75b5435f06a2cc
kkellerlbl <kkeller@lbl.gov>
2018-06-08 15:40:36 -0700

    Update htcondorstatus.py

--- htcondorstatus.py
+++ htcondorstatus.py
@@ -75,7 +75,7 @@ for slot in slots:
 	print str(slotState) + ' Condor_slot_' + slot['Name'] + ' state=' + str(slot['Activity']) + ' ' + slotStateText + ' - slot ' + slot['Name'] + ' in clientgroup ' + slot['CLIENTGROUP'] + ' is in state ' + slot['Activity']
 	# need to check for this key, and create if not exists
 	if slot['Activity'] not in slotCounts:
-		slotCounts[slot['Activity']] = {}
+		slotCounts[slot['Activity']] = []
 	if slot['CLIENTGROUP'] not in slotCounts[slot['Activity']:
 		slotCounts[slot['Activity']][slot['CLIENTGROUP']] = 1
 	else:
