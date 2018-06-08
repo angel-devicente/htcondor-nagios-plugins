ee0a42462d3031190354be675701b073a9ee807e
kkellerlbl <kkeller@lbl.gov>
2018-06-08 16:09:21 -0700

    Update htcondorstatus.py

--- htcondorstatus.py
+++ htcondorstatus.py
@@ -104,7 +104,7 @@ for clientgroup in conf.sections():
 			clientgroupState=2
 			clientgroupStateText='CRITICAL'
 
-		print str(clientgroupState) + ' Condor_clientgroup_' + clientgroup + ' - ' + clientgroupStateText + ' - clientgroup ' + clientgroup + ' has ' + str(slotCounts[clientgroup]['Total'] + ' total workers and' + str(slotCounts[clientgroup]['Idle'] + ' idle workers'
+		print str(clientgroupState) + ' Condor_clientgroup_' + clientgroup + ' - ' + clientgroupStateText + ' - clientgroup ' + clientgroup + ' has ' + str(slotCounts[clientgroup]['Total']) + ' total workers and' + str(slotCounts[clientgroup]['Idle'] + ' idle workers'
 		
 	except:
 		print str(3) + ' Condor_clientgroup_' + clientgroup + ' - UNKNOWN - clientgroup ' + clientgroup + ' has no workers in any state'
