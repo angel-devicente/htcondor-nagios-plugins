965a8ab8f76985688844925cdc62624724ef6f0b
kkellerlbl <kkeller@lbl.gov>
2018-06-08 15:52:15 -0700

    Update htcondorstatus.py

--- htcondorstatus.py
+++ htcondorstatus.py
@@ -72,12 +72,17 @@ for slot in slots:
 		
 	print str(slotState) + ' Condor_slot_' + slot['Name'] + ' state=' + str(slot['Activity']) + ' ' + slotStateText + ' - slot ' + slot['Name'] + ' in clientgroup ' + slot['CLIENTGROUP'] + ' is in state ' + slot['Activity']
 	# need to check for this key, and create if not exists
-	if slot['Activity'] not in slotCounts:
-		slotCounts[slot['Activity']] = {}
-	if slot['CLIENTGROUP'] not in slotCounts[slot['Activity']]:
-		slotCounts[slot['Activity']][slot['CLIENTGROUP']] = 1
+	if slot['CLIENTGROUP'] not in slotCounts:
+		slotCounts[slot['CLIENTGROUP']] = {}
+	if slot['Activity'] not in slotCounts[slot['CLIENTGROUP']]:
+		slotCounts[slot['CLIENTGROUP']][slot['Activity']] = 1
 	else:
-		slotCounts[slot['Activity']][slot['CLIENTGROUP']] += 1
+		slotCounts[slot['CLIENTGROUP']][slot['Activity']] += 1
+
+for clientgroup in conf.sections():
+	if clientgroup in ['DEFAULT','global']:
+		continue
+	print slotCounts[clientgroup]
 
 schedddaemon = collector.locateAll(htcondor.DaemonTypes.Schedd)[0]
 
